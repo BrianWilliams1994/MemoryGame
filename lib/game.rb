8ac0100e4417b31dfa97b0cbcf8ae8d45ee34396
Emily Rosengren <erosengr@thoughtworks.com>
2013-07-29 23:33:25 -0500

    adding game class

---
+++ lib/game.rb
@@ -0,0 +1,11 @@
+class Game
+
+  def initialize
+    @tile_values = ['Brian', 'Emily', 'Joe', 'Ben']
+  end
+
+  def rows
+    [[]]
+  end
+
+end
--- memory_game.rb
+++ memory_game.rb
@@ -1,5 +1,20 @@
+require './lib/game.rb'
 require 'sinatra'
 
+configure do
+  @@game = Game.new
+end
+
+helpers do
+  def game
+    @@game
+  end
+end
+
 get '/' do
+  erb :index
+end
+
+post '/guess' do
   erb :index
 end
