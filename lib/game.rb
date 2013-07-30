7dd432660d245cf298f8e1d449a96e5fc6aac2ae
Emily Rosengren <erosengr@thoughtworks.com>
2013-07-30 12:39:53 -0500

    adding hints to game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,15 @@
 class Game
 
   def initialize
-    @tile_values = ['Brian', 'Emily', 'Joe', 'Ben']
+    @tile_values = ['Brian', 'Emily', 'Joe', 'Ben', 'Josh', 'Miguel', 'Matt', 'Phil']
   end
 
   def rows
-    [[]]
+    # Your code here
+    # create the board randomly
+    # each tile value shoudl appear twice in the finished board
+    # see http://www.ruby-doc.org/core-1.9.3/Array.html
+    [['Brian', 'Emily'], ['Brian', 'Emily']]
   end
 
 end
