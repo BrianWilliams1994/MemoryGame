4399fae9544f9dd19c22646228b357276665f189
Emily Rosengren <erosengr@thoughtworks.com>
2013-08-07 07:52:00 -0500

    display board randomly

--- lib/game.rb
+++ lib/game.rb
@@ -1,20 +1,24 @@
+
 class Game
+  NUMBER_OF_ROWS = 4
+  NUMBER_OF_COLUMNS = 4
 
   def initialize
     @tile_values = ['Brian', 'Emily', 'Joe', 'Ben', 'Josh', 'Miguel', 'Matt', 'Phil']
   end
 
-  def rows
-      
-      ['Brian', 'Emily', 'Joe', 'Ben', 'Josh', 'Miguel', 'Matt', 'Phil'] * 2
+  def build_grid
+    all_tiles = (@tile_values * 2).shuffle
+    all_tiles = all_tiles.map { |name| Tile.new(name)}
+    @grid = []
+    NUMBER_OF_ROWS.times do 
+      @grid << all_tiles.pop(NUMBER_OF_COLUMNS)
+    end
+    @grid
+  end
 
-      @tile_values.shuffle
-      
-    # Your code here
-    # create the board randomly
-    # each tile value shoudl appear twice in the finished board
-    # see http://www.ruby-doc.org/core-1.9.3/Array.html
-    [['Brian', 'Emily'], ['Brian', 'Emily']]
+  def grid
+    @grid ||= build_grid
   end
 
 end
